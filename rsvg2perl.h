d39df99af7f87b7e426c7c1477aa74a612bfc9ac
Torsten Schönfeld <kaffeetisch@gmx.de>
2010-05-17 22:02:36 +0200

    Update copyright statements

--- rsvg2perl.h
+++ rsvg2perl.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003-2005 by the gtk2-perl team
+ * Copyright (C) 2003-2005, 2010  Torsten Schoenfeld
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -14,8 +14,6 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * $Id$
  */
 
 #ifndef _RSVG2PERL_H_
